308c6d19c5ca412a40d0da757c619c7d75fe92b2
Viktor Dorokhov <superdva@yandex.ru>
2024-04-20 15:54:11 +0300

    add Rollbar

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -4,12 +4,20 @@ import i18next from 'i18next';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
 import { io } from 'socket.io-client';
 import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './slices/index';
 import resources from './locales/index';
 import App from './components/App';
 import { messagesApi } from './services/messagesApi';
 import { channelsApi } from './services/channelsApi';
 import { setActiveChannel, defaultChannelId } from './slices/uiSlice';
+
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
+  enabled: process.env.NODE_ENV === 'production',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
 
 const init = async () => {
   const i18nInstance = i18next.createInstance();
@@ -56,11 +64,15 @@ const init = async () => {
   });
 
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18nInstance}>
-        <App />
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18nInstance}>
+            <App />
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
